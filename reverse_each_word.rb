5778571ebae9c7acdb234eeff7999f542cabacaf
Adriana Guerra <adriana.luz.guerra@gmail.com>
2019-06-07 20:15:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,9 @@ def reverse_each_word(string)
   arr.each{|w| s = s + " " + w.reverse}
   return s[1,s.length]
 end
+def reverse_each_word(string)
+  arr = string.split(' ')
+  s = "";
+  arr.collect{|w| s = s + " " + w.reverse}
+  return s[1,s.length]
+end
