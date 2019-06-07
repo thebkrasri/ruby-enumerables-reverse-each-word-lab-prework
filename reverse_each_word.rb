b909114f81094f04340ad23b440102730fad91e4
Adriana Guerra <adriana.luz.guerra@gmail.com>
2019-06-07 20:18:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,6 +6,7 @@ def reverse_each_word(string)
 end
 def reverse_each_word(string)
   arr = string.split(' ')
-  arr.collect{|w| w.reverse}
-  return arr
+  s = "";
+  arr.collect{|w| s = s + " " + w.reverse}
+  return s[1,s.length]
 end
